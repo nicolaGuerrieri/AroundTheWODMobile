bab0983939dd9077a52907398a5772e9678ae446
nicolaGuerrieri <guerrierinicola@gmail.com>
2017-03-27 18:04:24 +0200

    logo

--- src/pages/dialog/dialogSocial.ts
+++ src/pages/dialog/dialogSocial.ts
@@ -42,74 +42,44 @@ export class DialogSocial {
 		this.viewCtrl.dismiss();
 	}
 	doInstagram() { 
-	   this.platform.ready() .then(() =>		  {
-			 SocialSharing.shareViaInstagram(this.message, this.image)
+	   this.platform.ready().then(() => {
+			SocialSharing.shareViaInstagram(this.message, this.image) 
 			 .then((data) => {
 				console.log('Shared via shareViaInstagram');
 			 }) .catch((err) => {
 				alert('Was not shared via Instagram' + err);
-			 });
-
-		  });
-   }
-//	https://forum.ionicframework.com/t/ionic-2-ionic-cloud-auth-google-auth-failing-12501/72967/3
-	
-	doSharePicker(){
-      this.platform.ready()
-      .then(() =>
-      {
-         SocialSharing.share(this.message, this.subject, this.image, this.url)
-         .then((data) =>
-         {
-            console.log('Shared via SharePicker');
-         })
-         .catch((err) =>
-         {
-            	alert('Not able to be shared via SharePicker ' + err);
-         });
-
-      });
+			 }); 
+		});
    }
    
+//	https://forum.ionicframework.com/t/ionic-2-ionic-cloud-auth-google-auth-failing-12501/72967/3	
+  
 	doShare() { 
 		try{
-		SocialSharing.share(this.message, this.subject, this.image, this.url).then((data) => {
-			 
-		}).catch((err) => {
-		  alert('Not able to be shared ' + err);
-		});
-			 
-			
+			SocialSharing.share(this.message, this.subject, this.image, this.url).then((data) => {
+				 
+			}).catch((err) => {
+			  alert('Not able to be shared ' + err);
+			});
 		}catch (e) {
 		   alert("error: " + e);
 		}
 	}
 	doFacebook() {
 		this.platform.ready().then(() => {
-			SocialSharing.shareViaFacebookWithPasteMessageHint(this.message, this.image, this.url, this.message).then((data) =>{
-			   alert('Shared via Facebook');
+			SocialSharing.shareViaFacebookWithPasteMessageHint(this.message, null, this.url, 'Prova testo').then((data) =>{
+			 
 			}).catch((err) =>			{
 			   alert('Was not shared via Facebook');
 			});
 		});
 	}
 	doTwitter() {
-		//this.platform.ready()
-		//	.then(() =>{
-		//		SocialSharing.canShareVia('com.apple.social.twitter', this.message, this.image, this.url).then((data) =>{
-
-			SocialSharing.shareViaTwitter(this.message, this.image, this.url).then((result) => {
-					 
-			}).catch((err) =>{
-				alert('Not able to be shared via twitter ' + err);
-			});
-			 
-			
-		//	});
-				//		}).catch(() => {
-		//		  alert("Error please contact AroundTheWOD support");
-		//		});
-		
+		SocialSharing.shareViaTwitter(this.message, this.image, this.url).then((result) => {
+				 
+		}).catch((err) =>{
+			alert('Not able to be shared via twitter ' + err);
+		});
 	}
  
 		back(){
