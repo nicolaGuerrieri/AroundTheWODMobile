edc7df86a4058c2499538603e466a3531bb3c3ec
System Administrator <root@MBP-di-Nicola.station>
2017-10-14 19:04:44 +0200

    cazzo

--- src/pages/ricerca/dettaglioOrganizzazioni.ts
+++ src/pages/ricerca/dettaglioOrganizzazioni.ts
@@ -24,8 +24,25 @@ export class DettaglioOrganizzazioni {
 	constructor(public navCtrl: NavController, public global: Global, public params: NavParams, public cittaLuogoService: CittaLuogoService, private modalCtrl: ModalController, public loading: LoadingController, public plt: Platform) {
 		this.luoghiOrganizzazione = params.get("luoghiOrganizzazione");
 		this.item = params.get("organizzazione");
+		if (this.luoghiOrganizzazione == null) {
+			this.caricaLuoghiPerOrg(this.item);
+		}
 	}
 
+	caricaLuoghiPerOrg(organizzazione) {
+		this.cittaLuogoService.getLuogoForIdOrganizzazione(organizzazione._id).then(data => {
+			if (data != "error") {
+				data.listaLuoghi.forEach(element => {
+					console.log(element);
+					this.cittaLuogoService.getLuogoForId(element.luogo_id).then(data => {
+						this.luoghiOrganizzazione.push(data);
+					});
+
+				});
+			}
+			this.loader.dismiss();
+		});
+	}
 	selectPlace(idPlace) {
 		this.navCtrl.push(Detail, {
 			idLuogo: idPlace
